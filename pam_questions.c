9a248037cccc17edd1cba099cba855857015988c
gioruas <89271266+gioruas@users.noreply.github.com>
2025-05-30 08:06:25 -0300

    pam_questions.c

--- pam_questions.c
+++ pam_questions.c
@@ -13,29 +13,22 @@
 #define MAX_LINE 128
 #define MAX_ANSWER 128
 
-// Remove o \\n no final da string
-void aparar(char *str)
-{
+void aparar(char *str){
     size_t len = strlen(str);
     if (len > 0 && str[len - 1] == '\n') str[len - 1] = '\0';
 }
 
-// Cria o diretório ~/.pam_questions, se não existir
 void configurarDiretorio(const char *home) {
     char caminho[512];
     snprintf(caminho, sizeof(caminho), "%s/%s", home, CONFIG_DIR);
-    mkdir(caminho, 0700); // Só o dono pode acessar
+    mkdir(caminho, 0700); 
 }
 
-// Caminho completo para ~/.pam_questions/config
-void configurarCaminho(const char *home, char *out_path, size_t size)
-{
+void configurarCaminho(const char *home, char *out_path, size_t size){
     printf(out_path, size, "%s/%s/%s", home, CONFIG_DIR, CONFIG_FILE);
 }
 
-// Pede uma entrada ao usuário
-int perguntaUsuario(pam_handle_t *pamh, const char *prompt, int echo, char *out, size_t out_size)
-{
+int perguntaUsuario(pam_handle_t *pamh, const char *prompt, int echo, char *out, size_t out_size){
     const struct pam_conv *conv;
     struct pam_message mensagem;
     const struct pam_message *mensagemPergunta;
@@ -57,9 +50,7 @@ int perguntaUsuario(pam_handle_t *pamh, const char *prompt, int echo, char *out,
     return PAM_SUCCESS;
 }
 
-// Lê pergunta e resposta do arquivo config
-int leituraArquivo(const char *filepath, char *pergunta, char *resposta)
-{
+int leituraArquivo(const char *filepath, char *pergunta, char *resposta){
     FILE *file = fopen(filepath, "r");
     if (!file) return -1;
     char linha[MAX_LINE];
@@ -82,22 +73,18 @@ int leituraArquivo(const char *filepath, char *pergunta, char *resposta)
     return 0;
 }
 
-// Salva pergunta e resposta no arquivo config
-int salvarArquivoPergunta(const char *filepath, const char *pergunta, const char *resposta)
-{
+int salvarArquivoPergunta(const char *filepath, const char *pergunta, const char *resposta){
     FILE *file = fopen(filepath, "w");
     if (!file) return -1;
 
     fprintf(file, "pergunta=%s\nresposta=%s\n", pergunta, resposta);
     fclose(file);
 
-    chmod(filepath, 0600); // Só o usuário pode ler/escrever
+    chmod(filepath, 0600); 
     return 0;
 }
 
-// Função principal de autenticação
-PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
-{
+PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv){
 
     const char *username;
     struct passwd *pwd;
@@ -110,8 +97,6 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
 
     if (pam_get_user(pamh, &username, NULL) != PAM_SUCCESS) return PAM_AUTH_ERR;
 
-    //Root não precisa da autenticação
-
     if (strcmp(username, "root") == 0)
         return PAM_SUCCESS;
 
@@ -122,7 +107,6 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
     configurarDiretorio(pwd->pw_dir);
     configurarCaminho(pwd->pw_dir, filepath, sizeof(filepath));
 
-    // Se o arquivo não existir, pede pergunta e resposta e salva
     if (access(filepath, F_OK) != 0)
     {
         char nova_pergunta[MAX_LINE], nova_resposta[MAX_ANSWER];
@@ -135,7 +119,6 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
         return PAM_SUCCESS;
     }
 
-    // Caso já exista, carregue e compare
     if (leituraArquivo(filepath, pergunta, resposta) != 0) return PAM_AUTH_ERR;
     if (perguntaUsuario(pamh, pergunta, 1, resposta_usuario, sizeof(resposta_usuario)) != PAM_SUCCESS) return PAM_AUTH_ERR;
 
@@ -147,19 +130,16 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
     return PAM_AUTH_ERR;
 }
 
-// Função obrigatória
 PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
     return PAM_SUCCESS;
 }
 
-// Função obrigatória
 PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
     return PAM_SUCCESS;
 }
 
-// Função obrigatória
 PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
     return PAM_SUCCESS;
